0a43c151bdbbcf096a5ef77dc385840e3e040503
raniaezzo <ezzo.rania@gmail.com>
2026-07-23 16:55:44 +0400

    a few extra plots to explore zscore

--- AnalysisCode/01_process_singlesubjectGLM/createTablePerLocalStim.m
+++ AnalysisCode/01_process_singlesubjectGLM/createTablePerLocalStim.m
@@ -307,6 +307,34 @@ polPO(isOblique) = mean([ ...
 finalTable.cart_oriasym_PCvPO = cartPC - cartPO;
 finalTable.pol_oriasym_PCvPO = polPC - polPO;
 
+%% Compute motion vs static per experiment
+
+% Get column names
+vars = finalTable.Properties.VariableNames;
+
+
+% Cartesian motion vs static
+% 8 motion-direction columns
+cartMotCols = vars(startsWith(vars,'cart_') & contains(vars,'localmotdir_') & ~contains(vars,'localmotdir_none'));
+
+% 4 static columns
+cartStatCols = vars(startsWith(vars,'cart_') & contains(vars,'localmotdir_none'));
+
+finalTable.cart_MotvStat = ...
+    mean(finalTable{:,cartMotCols},2,'omitnan') - ...
+    mean(finalTable{:,cartStatCols},2,'omitnan');
+
+% Polar motion vs static
+% 8 motion-direction columns
+polMotCols = vars(startsWith(vars,'pol_') & contains(vars,'localmotdir_') & ~contains(vars,'localmotdir_none'));
+
+% 4 static columns
+polStatCols = vars(startsWith(vars,'pol_') & contains(vars,'localmotdir_none'));
+
+finalTable.pol_MotvStat = ...
+    mean(finalTable{:,polMotCols},2,'omitnan') - ...
+    mean(finalTable{:,polStatCols},2,'omitnan');
+
 
 %% Replot polar plot with horizontal vs vertical
 
@@ -632,7 +660,7 @@ writetable(finalTable, filename);
 
 %%
 
-function plotAsymmetry(finalTable, asymColCart, asymColPol, titleText)
+function plotAsymmetryCombinedSubjects(finalTable, asymColCart, asymColPol, titleText)
 
 idx = strcmp(finalTable.visual_area,'V1') & finalTable.included == 1;
 
@@ -670,14 +698,12 @@ for i = 1:4
     yline(0,'w-','LineWidth',1)
     hold on
 
-    histogram2(x, y, xedges, yedges, 'DisplayStyle', 'tile', 'ShowEmptyBins','on'); 
-    %clim([0 250])
-    set(gca, "ZScale", 'linear')
-    %cx = colorbar();
-    %cx.Ruler.Scale = 'log';
-    %h=scatter(x,y,10,'filled');
-%     h.MarkerFaceAlpha = 0.3;
-%     h.MarkerEdgeAlpha = 0.3;
+    %histogram2(x, y, xedges, yedges, 'DisplayStyle', 'tile', 'ShowEmptyBins','on'); 
+    %set(gca, "ZScale", 'linear')
+
+    h=scatter(x,y,10,'filled');
+    h.MarkerFaceAlpha = 0.3;
+    h.MarkerEdgeAlpha = 0.3;
 
 %     plot(median(x,'omitnan'), median(y,'omitnan'), ...
 %         'k+', 'MarkerSize',16,'LineWidth',2)
@@ -695,6 +721,340 @@ sgtitle(strcat(titleText, ' per V1 voxel (ecc 4-8, R^2>.1)'))
 end
 
 
+%%
 
 
+function plotAsymmetryPersubject(finalTable, asymColCart, asymColPol, titleText)
+
+betaType = 'std';
+
+% Included V1 voxels
+idx = strcmp(finalTable.visual_area,'V1') & finalTable.included == 1;
+
+T = finalTable(idx,:);
+
+subjects = unique(T.subject,'stable');
+nSubjects = numel(subjects);
+
+figure;
+
+% Choose beta variable
+switch betaType
+    case 'mean'
+        dgBeta = 'dg_beta_mean';
+        daBeta = 'da_beta_mean';
+        xlimVals = [-3 3];
+    case 'std'
+        dgBeta = 'dg_beta_std';
+        daBeta = 'da_beta_std';
+        xlimVals = [0 3];
+end
+
+% Histogram bins
+xedges = linspace(xlimVals(1), xlimVals(2), 41);
+yedges = linspace(-3,3,41);
+
+for s = 1:nSubjects
+
+    subjIdx = strcmp(T.subject, subjects{s});
+
+    % DG row
+    subplot(2,nSubjects,s)
+
+    x = T.(dgBeta)(subjIdx);
+    y = T.(asymColCart)(subjIdx);
+
+    histogram2(x,y,xedges,yedges,...
+        'DisplayStyle','tile',...
+        'ShowEmptyBins','on');
+
+    hold on
+    xline(0,'w-','LineWidth',1,'HandleVisibility','off')
+    yline(0,'w-','LineWidth',1,'HandleVisibility','off')
+    hold off
+
+    xlim(xlimVals)
+    ylim([-3 3])
+
+    title(subjects{s})
+    
+    if s == 1
+        ylabel('DG asymmetry')
+    end
+
+
+    % DA row
+    subplot(2,nSubjects,nSubjects+s)
+
+    x = T.(daBeta)(subjIdx);
+    y = T.(asymColPol)(subjIdx);
+
+    histogram2(x,y,xedges,yedges,...
+        'DisplayStyle','tile',...
+        'ShowEmptyBins','on');
+
+    hold on
+    xline(0,'w-','LineWidth',1,'HandleVisibility','off')
+    yline(0,'w-','LineWidth',1,'HandleVisibility','off')
+    hold off
+
+    xlim(xlimVals)
+    ylim([-3 3])
+
+    if s == 1
+        ylabel('DA asymmetry')
+    end
+
+end
+
+% Same color scale across all plots
+clim([0 20])   % adjust depending on max counts
+
+sgtitle(sprintf('%s %s per subject', titleText, betaType))
+
+end
+
+%%
+
+% function plotAsymmetry(finalTable, asymColCart, asymColPol, titleText)
+% 
+% betaType = 'std';
+% 
+% % Included V1 voxels
+% idx = strcmp(finalTable.visual_area,'V1') & finalTable.included == 1;
+% 
+% T = finalTable(idx,:);
+% 
+% subjects = unique(T.subject,'stable');
+% nSubjects = numel(subjects);
+% 
+% % Choose beta variable
+% switch betaType
+%     case 'mean'
+%         dgBeta = 'dg_beta_mean';
+%         daBeta = 'da_beta_mean';
+%         xlimVals = [-3 3];
+%     case 'std'
+%         dgBeta = 'dg_beta_std';
+%         daBeta = 'da_beta_std';
+%         xlimVals = [0 3];
+% end
+% 
+% % pRF angle colors
+% angleBins = 0:45:315;
+% colors = hsv(numel(angleBins));
+% 
+% figure;
+% 
+% for s = 1:nSubjects
+% 
+%     subjIdx = strcmp(T.subject, subjects{s});
+% 
+%     % DG row
+%     subplot(2,nSubjects,s)
+% 
+%     x = T.(dgBeta)(subjIdx);
+%     y = T.(asymColCart)(subjIdx);
+%     angle = T.pRF_angle_bin(subjIdx);
+% 
+%     hold on
+% 
+%     for a = 1:numel(angleBins)
+% 
+%         aIdx = angle == angleBins(a);
+% 
+%         scatter(x(aIdx), y(aIdx), ...
+%             10, colors(a,:), 'filled', ...
+%             'MarkerFaceAlpha',0.3,...
+%             'MarkerEdgeAlpha',0.3);
+% 
+%     end
+% 
+%     xline(0,'k-','LineWidth',1,'HandleVisibility','off')
+%     yline(0,'k-','LineWidth',1,'HandleVisibility','off')
+% 
+%     hold off
+% 
+%     xlim(xlimVals)
+%     ylim([-3 3])
+% 
+%     title(subjects{s})
+% 
+%     if s == 1
+%         ylabel('DG asymmetry')
+%     end
+% 
+% 
+%     % DA row
+%     subplot(2,nSubjects,nSubjects+s)
+% 
+%     x = T.(daBeta)(subjIdx);
+%     y = T.(asymColPol)(subjIdx);
+%     angle = T.pRF_angle_bin(subjIdx);
+% 
+%     hold on
+% 
+%     for a = 1:numel(angleBins)
+% 
+%         aIdx = angle == angleBins(a);
+% 
+%         scatter(x(aIdx), y(aIdx), ...
+%             10, colors(a,:), 'filled', ...
+%             'MarkerFaceAlpha',0.3,...
+%             'MarkerEdgeAlpha',0.3);
+% 
+%     end
+% 
+%     xline(0,'k-','LineWidth',1,'HandleVisibility','off')
+%     yline(0,'k-','LineWidth',1,'HandleVisibility','off')
+% 
+%     hold off
+% 
+%     xlim(xlimVals)
+%     ylim([-3 3])
+% 
+%     if s == 1
+%         ylabel('DA asymmetry')
+%     end
+% 
+% end
+% 
+% 
+% % Add one shared legend
+% figure(gcf)
+% hold on
+% h = gobjects(numel(angleBins),1);
+% 
+% for a = 1:numel(angleBins)
+%     h(a) = scatter(nan,nan,30,colors(a,:),'filled');
+% end
+% 
+% legend(h,string(angleBins)+"°",...
+%     'Location','eastoutside')
+% 
+% sgtitle(sprintf('%s %s per subject (colored by pRF angle)', ...
+%     titleText,betaType))
+% 
+% end
+
+%%
+
+function plotAsymmetry(finalTable, asymColCart, asymColPol, titleText)
+
+betaType = 'std';
+
+% Included V1 voxels
+idx = strcmp(finalTable.visual_area,'V1') & finalTable.included == 1;
+
+T = finalTable(idx,:);
+
+subjects = unique(T.subject,'stable');
+nSubjects = numel(subjects);
+
+% Choose beta variable
+switch betaType
+    case 'mean'
+        dgBeta = 'dg_beta_mean';
+        daBeta = 'da_beta_mean';
+        xlimVals = [-3 3];
+    case 'std'
+        dgBeta = 'dg_beta_std';
+        daBeta = 'da_beta_std';
+        xlimVals = [0 4];
+end
+
+figure;
+
+for s = 1:nSubjects
+
+    subjIdx = strcmp(T.subject, subjects{s});
+
+
+    % DG row (Cartesian)
+    subplot(2,nSubjects,s)
+
+    x = T.(dgBeta)(subjIdx);
+    y = T.(asymColCart)(subjIdx);
+    motStat = T.cart_MotvStat(subjIdx);
+
+    h = plotMotHighlight(x,y,motStat);
+    legend({'Remaining voxels','',''})
+    
+
+    xline(0,'k-','LineWidth',1,'HandleVisibility','off')
+    yline(0,'k-','LineWidth',1,'HandleVisibility','off')
+
+    xlim(xlimVals)
+    ylim([-3 3])
+
+    title(subjects{s})
+
+    if s == 1
+        legend(h, {'Remaining voxels','Bottom 20% motion-selective','Top 20% motion-selective'}, ...
+            'Location','best');
+        ylabel('DG asymmetry')
+    else
+        legend('off');
+    end
+
+
+    % DA row (Polar)
+    subplot(2,nSubjects,nSubjects+s)
+
+    x = T.(daBeta)(subjIdx);
+    y = T.(asymColPol)(subjIdx);
+    motStat = T.pol_MotvStat(subjIdx);
+
+    plotMotHighlight(x,y,motStat);
+
+    xline(0,'k-','LineWidth',1,'HandleVisibility','off')
+    yline(0,'k-','LineWidth',1,'HandleVisibility','off')
+
+    xlim(xlimVals)
+    ylim([-3 3])
+
+    if s == 1
+        ylabel('DA asymmetry')
+    end
+
+end
+
+sgtitle(sprintf('%s %s per subject (highlighting motion sensitivity)', ...
+    titleText,betaType))
+f = gcf;
+
+f.Position = [32 274 2153 719];
+end
+
+
+function h = plotMotHighlight(x,y,motStat)
+
+% percentile thresholds
+lowThresh = prctile(motStat,20);
+highThresh = prctile(motStat,80);
+
+lowIdx = motStat <= lowThresh;
+highIdx = motStat >= highThresh;
+midIdx = ~lowIdx & ~highIdx;
+
+hold on
+
+% Middle 60%
+h(1) = scatter(x(midIdx),y(midIdx),10,[0.3 0.3 0.3],'filled',...
+    'MarkerFaceAlpha',0.3,...
+    'MarkerEdgeAlpha',0.3);
+
+% Lowest 20%
+h(2) = scatter(x(lowIdx),y(lowIdx),10,[0 0.45 1],'filled',...
+    'MarkerFaceAlpha',0.5,...
+    'MarkerEdgeAlpha',0.5);
+
+% Highest 20%
+h(3) = scatter(x(highIdx),y(highIdx),10,[1 0 0],'filled',...
+    'MarkerFaceAlpha',0.5,...
+    'MarkerEdgeAlpha',0.5);
+
+hold off
+
+end
 
